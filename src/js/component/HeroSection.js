9847d16a08efb1f8ead9579929fb9738c1475696
Jose Hurtado <hello@hurtadojose.com>
2022-10-26 04:40:11 +0000

    Hero section Typer

--- src/js/component/HeroSection.js
+++ src/js/component/HeroSection.js
@@ -1,13 +1,20 @@
 import React from "react";
+import ProfilePic from "/src/img/Image_JoseHurtado.jpg"
 
 export function HeroSection() {
 	return (
-<section className="container">
-    <div className="row py-lg-5">
-      <div className="col-lg-6 col-md-8 mx-auto">
-        <h1 className="fw-light"><strong>Hola, I’m Jose Hurtado,full-stack designer specializedin `{`User Experience`}` design, based in Miami.</strong></h1>
-      </div>
+
+<section className="container HeroSection">
+  <div className="row">
+    <div id="HeadlineHero" className="col-6">
+    <h1>Hola, I’m Jose Hurtado, full-stack designer specialized in &#123;
+      <span className="typer" id="main" data-colors="#001648" data-words=" User Experience, User Interface, Motion, Logo, Web" data-delay="100" data-deleteDelay="1000"></span> &#125; design, based in Miami
+    </h1>
     </div>
-  </section>
+    <div className="col-4">
+      <img className="profileImage" src={ProfilePic} alt="Profile image" />
+      </div>
+  </div>
+</section>
 	);
 }
