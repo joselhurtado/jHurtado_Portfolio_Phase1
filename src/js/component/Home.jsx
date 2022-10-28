ab0840474ba8d327b2c2fbf0cf5b38ed23ad613b
Jose Hurtado <hello@hurtadojose.com>
2022-10-28 21:50:57 +0000

    toolbox edits

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { Navbar } from "./Navbar.js";
 import { HeroSection } from "./HeroSection.js";
+import { ToolBoxSection } from "./ToolBoxSection.js";
 import { Footer } from "./Footer.js";
 
 
 export function Home() {
   return (
-    <div className="container">
+    <div className="container-fluid">
       <div className="container">
         <Navbar />
       </div>
+
       <div className="container">
         <HeroSection />
       </div>
 
-      <div className="container">
+      <div>
+        <ToolBoxSection />
       </div>
-      <div className="container">
+
+      <div>
         <Footer />
       </div>
     </div>
